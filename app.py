430eaa67e7a8823db6abbce3eb52df11920264e2
martindustry <marcin.exn@gmail.com>
2022-06-09 19:26:37 +0200

    Add user session
    * Add user to session after login/register
    
    * Redirect not logged user to login form
    
    * Handle flash messages which pop up on users screen

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, session, flash, get_flashed_messages
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -7,6 +7,9 @@ app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 DB_NAME = "database.db"
+
+# TODO: Change it to env variable
+app.config['SECRET_KEY'] = 'supersecretkeythatnobodycansee'
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_NAME}"
 db = SQLAlchemy(app)
 
@@ -20,26 +23,30 @@ db.session.commit()
 
 @app.route('/')
 def index():
+    if not 'user_id' in session:
+        return redirect('/login')
     return render_template('layout.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if 'user_id' in session:
+        return redirect('/')
+
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
 
         user = User.query.filter_by(username=username).first()
         if not user:
-            # TODO: flash message
-            print("user not found")
+            flash("User not found", category='error')
             return render_template('login.html')
         if not check_password_hash(user.password, password):
-            # TODO: flash message
-            print("wrong password")
+            flash("Wrong password", category='error')
             return render_template('login.html')
         
-        print("successfully logged in")
+        session['user_id'] = user.id
+        flash(f"Successfully logged in!", category='success')
         return redirect('/')
 
     return render_template('login.html')
@@ -47,41 +54,51 @@ def login():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if 'user_id' in session:
+        return redirect('/')
+
     if request.method == 'POST':
+
         username = request.form.get('username')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
         if len(username) < 3:
-            # TODO: flash message
-            print("username too short")
+            flash("Username is too short", category='error')
             return render_template('register.html')
         if len(username) >= 20:
-            print("username too long")
+            flash("Username is too long", category='error')
             return render_template('register.html')
         if password1 != password2:
-            # TODO: flash message
-            print("passwords don't match")
+            flash("Passwords don't match", category='error')
             return render_template('register.html')
         if len(password1) < 6:
-            # TODO: flash message
-            print("password must be at least 6 characters")
+            flash("Password must be at least 6 characters", category='error')
             return render_template('register.html')
         if len(password1) >= 50:
-            print("password is too long")
+            flash("Password is too long", category='error')
             return render_template('register.html')
 
         found_user = User.query.filter_by(username=username).first()
         if found_user:
-             # TODO: flash message
-             print("username already exists")
-             return render_template('register.html')
+            flash("Username already exists", category='error')
+            return render_template('register.html')
 
         password = generate_password_hash(password1, method='sha256')
-        new_user = User(username=username, password=password)
-        db.session.add(new_user)
+        user = User(username=username, password=password)
+        db.session.add(user)
         db.session.commit()
-        # TODO: flash message
+        
+        session['user_id'] = user.id
+        flash("Successfully registered!", category='success')
         return redirect('/')
 
     return render_template('register.html')
+
+
+@app.route('/logout')
+def logout():
+    if 'user_id' in session:
+        session.clear()
+        flash("Successfully logged out", category='success')
+    return redirect('/')
