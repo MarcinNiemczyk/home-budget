1998be5f101307f0940a07cd804dac15adc39b78
martindustry <marcin.exn@gmail.com>
2022-06-09 13:32:41 +0200

    Add login forms
    * Add login route for get method
    Add front-end for login form
    
    * Add register route for get method
    Add front-end for register form

--- app.py
+++ app.py
@@ -8,3 +8,13 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 @app.route('/')
 def index():
     return render_template('layout.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
