a653a81e53f9f26b53666c699d579d1249d485f4
martindustry <marcin.exn@gmail.com>
2022-06-17 16:25:53 +0200

    Add global style for select menu

--- static/script.js
+++ static/script.js
@@ -1,3 +1,4 @@
+// Navbar toggle
 const NAVBAR = document.querySelector('nav')
 
 document.querySelector('.toggle-menu').addEventListener('click', () => {
@@ -8,3 +9,32 @@ document.querySelector('.navbar__close').addEventListener('click', () => {
     NAVBAR.classList.remove('active')
 })
 
+// Dropdown menu
+const SELECTED = document.querySelector('.select__selected')
+const OPTIONS_CONTAINER = document.querySelector('.select__options')
+const OPTIONS = document.querySelectorAll('.select__option')
+
+// Toggle visibility of dropdown menu
+SELECTED.addEventListener('click', () => {
+    OPTIONS_CONTAINER.classList.toggle('active')
+})
+
+OPTIONS.forEach(option => {
+    option.addEventListener('click', () => {
+        // Change selected filter value
+        SELECTED.innerHTML = option.innerHTML
+
+        // Remove active style effect from previous option
+        OPTIONS.forEach(option => {
+            option.classList.remove('active')
+        })
+        
+        // Add active style effect to new option
+        option.classList.add('active')
+
+        // Close dropdown menu
+        OPTIONS_CONTAINER.classList.remove('active')
+    })
+})
+
+// TODO: Change display to none for every unselected item
