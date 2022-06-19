0203c4af14c7955b76149fce6ea297a0330e5e5f
martindustry <marcin.exn@gmail.com>
2022-06-19 16:18:45 +0200

    Refactor script.js
    * Make dropdown menu function which corresponds with multiple menus

--- static/script.js
+++ static/script.js
@@ -9,123 +9,25 @@ document.querySelector('.navbar__close').addEventListener('click', () => {
     NAVBAR.classList.remove('active')
 })
 
-// Track current page route
-let currentPage = window.location.pathname
-
-if (currentPage == '/transactions') {
-        // Filter dropdown menu
-        const SELECTED_FILTER = document.querySelector('.filter__selected')
-        const FILTER_CONTAINER = document.querySelector('.filter')
-        const FILTER = document.querySelectorAll('.filter__option label')
-    
-        // Toggle visibility of dropdown menu
-        SELECTED_FILTER.addEventListener('click', () => {
-            FILTER_CONTAINER.classList.toggle('active')
-        })
-    
-        FILTER.forEach(option => {
-            option.addEventListener('click', () => {
-                // Change selected filter value
-                SELECTED_FILTER.innerHTML = option.innerHTML
-    
-                // Remove active style effect from previous option
-                FILTER.forEach(option => {
-                    option.classList.remove('active')
-                })
-                
-                // Add active style effect to new option
-                option.classList.add('active')
-    
-                // Close dropdown menu
-                FILTER_CONTAINER.classList.remove('active')
-            })
-        })
-    
-    
-        // const FILTERS = document.querySelectorAll('input[name="filter__radio"]')
-    
-        // FILTERS.forEach(filter => {
-        //     filter.addEventListener('change', () => {
-        //         if (filter.checked) {
-        //             console.log(filter.value + ' is checked')
-        //         }
-        //     })
-        // })
-    
-    
-        // Months dropdown menu
-        const SELECTED_MONTH = document.querySelector('.months__selected')
-        const MONTHS_CONTAINER = document.querySelector('.months')
-        const MONTHS = document.querySelectorAll('.months__option label')
-    
-        // Toggle visibility of months dropdown menu
-        SELECTED_MONTH.addEventListener('click', () => {
-            MONTHS_CONTAINER.classList.toggle('active')
-        })
-    
-        MONTHS.forEach(option => {
-            option.addEventListener('click', () => {
-                // Change selected month value
-                SELECTED_MONTH.innerHTML = option.innerHTML
-    
-                // Remove active style effect from previous option
-                MONTHS.forEach(option => {
-                    option.classList.remove('active')
-                })
-                
-                // Add active style effect to new option
-                option.classList.add('active')
-    
-                // Close dropdown menu
-                MONTHS_CONTAINER.classList.remove('active')
-            })
-        })
-    
-        // Years dropdown menu
-        const SELECTED_YEAR = document.querySelector('.years__selected')
-        const YEARS_CONTAINER = document.querySelector('.years')
-        const YEARS = document.querySelectorAll('.years__option label')
-    
-        // Toggle visibility of months dropdown menu
-        SELECTED_YEAR.addEventListener('click', () => {
-            YEARS_CONTAINER.classList.toggle('active')
-        })
-    
-        YEARS.forEach(option => {
-            option.addEventListener('click', () => {
-                // Change selected month value
-                SELECTED_YEAR.innerHTML = option.innerHTML
-    
-                // Remove active style effect from previous option
-                YEARS.forEach(option => {
-                    option.classList.remove('active')
-                })
-                
-                // Add active style effect to new option
-                option.classList.add('active')
-    
-                // Close dropdown menu
-                YEARS_CONTAINER.classList.remove('active')
-            })
-        })
-} else if (currentPage == '/transactions/add') {
-    // Category dropdown menu
-    const SELECTED_CATEGORY = document.querySelector('.category__selected')
-    const CATEGORY_CONTAINER = document.querySelector('.category')
-    const CATEGORY = document.querySelectorAll('.category__option label')
+// Handle dropdown menu display
+function dropdownMenu(NAME) {
+    const SELECTED = document.querySelector('.' + NAME + '__selected')
+    const OPTIONS_CONTAINER = document.querySelector('.' + NAME)
+    const OPTIONS = document.querySelectorAll('.' + NAME+ '__option label')
 
     // Toggle visibility of dropdown menu
-    SELECTED_CATEGORY.addEventListener('click', () => {
-        CATEGORY_CONTAINER.classList.toggle('active')
+    SELECTED.addEventListener('click', () => {
+        OPTIONS_CONTAINER.classList.toggle('active')
     })
 
-    CATEGORY.forEach(option => {
+    // Handle options interaction
+    OPTIONS.forEach(option => {
         option.addEventListener('click', () => {
-            // Change selected category value
-            SELECTED_CATEGORY.innerHTML = option.innerHTML
+            // Change selected option value
+            SELECTED.innerHTML = option.innerHTML
 
             // Remove active style effect from previous option
-            CATEGORY.forEach(option => {
+            OPTIONS.forEach(option => {
                 option.classList.remove('active')
             })
             
@@ -133,8 +35,19 @@ if (currentPage == '/transactions') {
             option.classList.add('active')
 
             // Close dropdown menu
-            CATEGORY_CONTAINER.classList.remove('active')
+            OPTIONS_CONTAINER.classList.remove('active')
         })
     })
 }
 
+// Track current page route
+let currentPage = window.location.pathname
+
+// Apply right functionality
+if (currentPage == '/transactions') {
+    dropdownMenu('filter')
+    dropdownMenu('months')
+    dropdownMenu('years')
+} else if (currentPage == '/transactions/add') {
+    dropdownMenu('category')
+}
