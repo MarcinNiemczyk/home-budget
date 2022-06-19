6e12892c6224b9bfa90b462f93a2b482558c827c
martindustry <marcin.exn@gmail.com>
2022-06-19 15:51:37 +0200

    Fix dropdown menu script
    * Add route dependencies

--- static/script.js
+++ static/script.js
@@ -9,127 +9,132 @@ document.querySelector('.navbar__close').addEventListener('click', () => {
     NAVBAR.classList.remove('active')
 })
 
+// Track current page route
+let currentPage = window.location.pathname
 
-// Category dropdown menu
-const SELECTED_CATEGORY = document.querySelector('.category__selected')
-const CATEGORY_CONTAINER = document.querySelector('.category')
-const CATEGORY = document.querySelectorAll('.category__option label')
-
-// Toggle visibility of dropdown menu
-SELECTED_CATEGORY.addEventListener('click', () => {
-    CATEGORY_CONTAINER.classList.toggle('active')
-})
-
-CATEGORY.forEach(option => {
-    option.addEventListener('click', () => {
-        // Change selected category value
-        SELECTED_CATEGORY.innerHTML = option.innerHTML
-
-        // Remove active style effect from previous option
-        CATEGORY.forEach(option => {
-            option.classList.remove('active')
+if (currentPage == '/transactions') {
+        // Filter dropdown menu
+        const SELECTED_FILTER = document.querySelector('.filter__selected')
+        const FILTER_CONTAINER = document.querySelector('.filter')
+        const FILTER = document.querySelectorAll('.filter__option label')
+    
+        // Toggle visibility of dropdown menu
+        SELECTED_FILTER.addEventListener('click', () => {
+            FILTER_CONTAINER.classList.toggle('active')
         })
-        
-        // Add active style effect to new option
-        option.classList.add('active')
-
-        // Close dropdown menu
-        CATEGORY_CONTAINER.classList.remove('active')
-    })
-})
-
-// Filter dropdown menu
-const SELECTED_FILTER = document.querySelector('.filter__selected')
-const FILTER_CONTAINER = document.querySelector('.filter')
-const FILTER = document.querySelectorAll('.filter__option label')
-
-// Toggle visibility of dropdown menu
-SELECTED_FILTER.addEventListener('click', () => {
-    FILTER_CONTAINER.classList.toggle('active')
-})
-
-FILTER.forEach(option => {
-    option.addEventListener('click', () => {
-        // Change selected filter value
-        SELECTED_FILTER.innerHTML = option.innerHTML
-
-        // Remove active style effect from previous option
+    
         FILTER.forEach(option => {
-            option.classList.remove('active')
+            option.addEventListener('click', () => {
+                // Change selected filter value
+                SELECTED_FILTER.innerHTML = option.innerHTML
+    
+                // Remove active style effect from previous option
+                FILTER.forEach(option => {
+                    option.classList.remove('active')
+                })
+                
+                // Add active style effect to new option
+                option.classList.add('active')
+    
+                // Close dropdown menu
+                FILTER_CONTAINER.classList.remove('active')
+            })
         })
-        
-        // Add active style effect to new option
-        option.classList.add('active')
-
-        // Close dropdown menu
-        FILTER_CONTAINER.classList.remove('active')
-    })
-})
-
-
-// const FILTERS = document.querySelectorAll('input[name="filter__radio"]')
-
-// FILTERS.forEach(filter => {
-//     filter.addEventListener('change', () => {
-//         if (filter.checked) {
-//             console.log(filter.value + ' is checked')
-//         }
-//     })
-// })
-
-
-// Months dropdown menu
-const SELECTED_MONTH = document.querySelector('.months__selected')
-const MONTHS_CONTAINER = document.querySelector('.months')
-const MONTHS = document.querySelectorAll('.months__option label')
-
-// Toggle visibility of months dropdown menu
-SELECTED_MONTH.addEventListener('click', () => {
-    MONTHS_CONTAINER.classList.toggle('active')
-})
-
-MONTHS.forEach(option => {
-    option.addEventListener('click', () => {
-        // Change selected month value
-        SELECTED_MONTH.innerHTML = option.innerHTML
-
-        // Remove active style effect from previous option
+    
+    
+        // const FILTERS = document.querySelectorAll('input[name="filter__radio"]')
+    
+        // FILTERS.forEach(filter => {
+        //     filter.addEventListener('change', () => {
+        //         if (filter.checked) {
+        //             console.log(filter.value + ' is checked')
+        //         }
+        //     })
+        // })
+    
+    
+        // Months dropdown menu
+        const SELECTED_MONTH = document.querySelector('.months__selected')
+        const MONTHS_CONTAINER = document.querySelector('.months')
+        const MONTHS = document.querySelectorAll('.months__option label')
+    
+        // Toggle visibility of months dropdown menu
+        SELECTED_MONTH.addEventListener('click', () => {
+            MONTHS_CONTAINER.classList.toggle('active')
+        })
+    
         MONTHS.forEach(option => {
-            option.classList.remove('active')
+            option.addEventListener('click', () => {
+                // Change selected month value
+                SELECTED_MONTH.innerHTML = option.innerHTML
+    
+                // Remove active style effect from previous option
+                MONTHS.forEach(option => {
+                    option.classList.remove('active')
+                })
+                
+                // Add active style effect to new option
+                option.classList.add('active')
+    
+                // Close dropdown menu
+                MONTHS_CONTAINER.classList.remove('active')
+            })
         })
-        
-        // Add active style effect to new option
-        option.classList.add('active')
-
-        // Close dropdown menu
-        MONTHS_CONTAINER.classList.remove('active')
-    })
-})
-
-// Years dropdown menu
-const SELECTED_YEAR = document.querySelector('.years__selected')
-const YEARS_CONTAINER = document.querySelector('.years')
-const YEARS = document.querySelectorAll('.years__option label')
-
-// Toggle visibility of months dropdown menu
-SELECTED_YEAR.addEventListener('click', () => {
-    YEARS_CONTAINER.classList.toggle('active')
-})
-
-YEARS.forEach(option => {
-    option.addEventListener('click', () => {
-        // Change selected month value
-        SELECTED_YEAR.innerHTML = option.innerHTML
-
-        // Remove active style effect from previous option
+    
+        // Years dropdown menu
+        const SELECTED_YEAR = document.querySelector('.years__selected')
+        const YEARS_CONTAINER = document.querySelector('.years')
+        const YEARS = document.querySelectorAll('.years__option label')
+    
+        // Toggle visibility of months dropdown menu
+        SELECTED_YEAR.addEventListener('click', () => {
+            YEARS_CONTAINER.classList.toggle('active')
+        })
+    
         YEARS.forEach(option => {
-            option.classList.remove('active')
+            option.addEventListener('click', () => {
+                // Change selected month value
+                SELECTED_YEAR.innerHTML = option.innerHTML
+    
+                // Remove active style effect from previous option
+                YEARS.forEach(option => {
+                    option.classList.remove('active')
+                })
+                
+                // Add active style effect to new option
+                option.classList.add('active')
+    
+                // Close dropdown menu
+                YEARS_CONTAINER.classList.remove('active')
+            })
         })
-        
-        // Add active style effect to new option
-        option.classList.add('active')
+} else if (currentPage == '/transactions/add') {
+    // Category dropdown menu
+    const SELECTED_CATEGORY = document.querySelector('.category__selected')
+    const CATEGORY_CONTAINER = document.querySelector('.category')
+    const CATEGORY = document.querySelectorAll('.category__option label')
 
-        // Close dropdown menu
-        YEARS_CONTAINER.classList.remove('active')
+    // Toggle visibility of dropdown menu
+    SELECTED_CATEGORY.addEventListener('click', () => {
+        CATEGORY_CONTAINER.classList.toggle('active')
     })
-})
+
+    CATEGORY.forEach(option => {
+        option.addEventListener('click', () => {
+            // Change selected category value
+            SELECTED_CATEGORY.innerHTML = option.innerHTML
+
+            // Remove active style effect from previous option
+            CATEGORY.forEach(option => {
+                option.classList.remove('active')
+            })
+            
+            // Add active style effect to new option
+            option.classList.add('active')
+
+            // Close dropdown menu
+            CATEGORY_CONTAINER.classList.remove('active')
+        })
+    })
+}
+
