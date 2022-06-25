9cd4d2ec9e70fa5d54e8956999ecad0e3dde8c53
martindustry <marcin.exn@gmail.com>
2022-06-25 11:44:54 +0200

    Add planning page layout

--- views/planning.py
+++ views/planning.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-from ..models import login_required
+from ..models import CATEGORIES, MONTHS, YEARS, login_required
 
 
 planning = Blueprint('planning', __name__)
@@ -9,4 +9,4 @@ planning = Blueprint('planning', __name__)
 def planning_page():
     """Show planned expenses and allow user to modify data"""
 
-    return render_template('planning/planning.html')
+    return render_template('planning/planning.html', outcomes=CATEGORIES['outcomes'], incomes=CATEGORIES['incomes'], years=YEARS, months=MONTHS, selected_month=6, selected_year=2022)
